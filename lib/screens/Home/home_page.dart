2d807183580ee85db77229c23925f7d830c4e1a7
hakou guelfen <hakoudev@gmail.com>
2022-04-15 01:51:04 +0100

    change app name

--- lib/screens/Home/home_page.dart
+++ lib/screens/Home/home_page.dart
@@ -2,125 +2,60 @@ import 'package:audio_service/audio_service.dart';
 import 'package:flutter/material.dart';
 import 'package:music_play/components/bottom_music_controller.dart';
 import 'package:music_play/manager/page_manager.dart';
+import 'package:music_play/screens/Home/all_songs.dart';
 import 'package:music_play/services/service_locator.dart';
 
 import 'package:music_play/constants.dart';
 
-import '../../components/music_container.dart';
-
-class HomePage extends StatefulWidget {
+class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
-  State<HomePage> createState() => _MusicListState();
-}
-
-class _MusicListState extends State<HomePage> {
-  int currentIndex = 0;
-
-  @override
   Widget build(BuildContext context) {
+    const List<Tab> myTabs = <Tab>[
+      Tab(child: Text('all songs')),
+      Tab(child: Text('artists')),
+    ];
     final pageManager = getIt<PageManager>();
-    List<String> songsTypes = ['artists', 'albums', 'genre'];
 
-    return Scaffold(
-      body: CustomScrollView(
-        slivers: [
-          SliverAppBar(
-            pinned: true,
-            snap: false,
-            floating: true,
-            expandedHeight: 160.0,
-            elevation: 0,
-            backgroundColor: Theme.of(context).scaffoldBackgroundColor,
-            title: Text(
-              'Mousika',
-              style:
-                  Theme.of(context).textTheme.headline6!.copyWith(fontSize: 18),
-            ),
-            leading: const Padding(
-              padding: EdgeInsets.all(defaultPadding * 0.5),
-              child: Image(image: AssetImage('assets/icons/logo.png')),
-            ),
-            flexibleSpace: FlexibleSpaceBar(
-              background: SingleChildScrollView(
-                scrollDirection: Axis.horizontal,
-                child: Row(
-                  children: [
-                    Container(
-                      width: 100,
-                      height: 40,
-                      margin: const EdgeInsets.only(top: 100, left: 10),
-                      child: const Center(
-                        child: Text(
-                          "all songs",
-                          style: TextStyle(color: Colors.black),
-                        ),
-                      ),
-                      decoration: BoxDecoration(
-                        color: Theme.of(context).colorScheme.secondary,
-                        borderRadius: BorderRadius.circular(100),
-                      ),
-                    ),
-                    SizedBox(
-                      height: 140,
-                      child: ListView.builder(
-                        itemBuilder: (context, index) {
-                          return Container(
-                            width: 100,
-                            margin: const EdgeInsets.only(top: 100, left: 10),
-                            child: Center(
-                              child: Text(songsTypes[index]),
-                            ),
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(100),
-                              border: Border.all(
-                                color: Theme.of(context)
-                                    .colorScheme
-                                    .secondaryContainer,
-                              ),
-                            ),
-                          );
-                        },
-                        itemCount: songsTypes.length,
-                        scrollDirection: Axis.horizontal,
-                        physics: const ClampingScrollPhysics(),
-                        shrinkWrap: true,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ),
-          SliverList(
-            delegate: SliverChildBuilderDelegate(
-              (BuildContext context, int index) {
-                return ValueListenableBuilder<List<MediaItem>>(
-                  valueListenable: pageManager.playlistNotifier,
-                  builder: (_, playlist, __) {
-                    return Padding(
-                      padding: const EdgeInsets.symmetric(
-                        horizontal: defaultPadding * 0.5,
-                      ),
-                      child: MusicContainer(
-                        currentSong: playlist[index],
-                        gotoNextPage: false,
-                      ),
-                    );
-                  },
-                );
-              },
-              childCount: 2,
-            ),
-          ),
-        ],
+    return DefaultTabController(
+      length: myTabs.length,
+      child: Scaffold(
+        appBar: buildAppBar(context, myTabs),
+        body: TabBarView(
+          children: const [
+            AllSongs(),
+            Text('artists'),
+          ],
+        ),
+        bottomNavigationBar: ValueListenableBuilder<MediaItem>(
+          valueListenable: pageManager.currentSongNotifier,
+          builder: (_, song, __) {
+            return BottomMusicController(currentSong: song);
+          },
+        ),
       ),
-      bottomNavigationBar: ValueListenableBuilder<MediaItem>(
-        valueListenable: pageManager.currentSongNotifier,
-        builder: (_, song, __) {
-          return BottomMusicController(currentSong: song);
-        },
+    );
+  }
+
+  AppBar buildAppBar(BuildContext context, List<Tab> myTabs) {
+    return AppBar(
+      elevation: 0,
+      backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+      title: Text(
+        'Mousika',
+        style: Theme.of(context).textTheme.headline6!.copyWith(fontSize: 18),
+      ),
+      leading: const Padding(
+        padding: EdgeInsets.all(defaultPadding * 0.5),
+        child: Image(image: AssetImage('assets/icons/logo.png')),
+      ),
+      bottom: TabBar(
+        indicator: BoxDecoration(
+          color: Theme.of(context).cardColor,
+          borderRadius: BorderRadius.circular(defaultBorderRaduis),
+        ),
+        tabs: myTabs,
       ),
     );
   }
